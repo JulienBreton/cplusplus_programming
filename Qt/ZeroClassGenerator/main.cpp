e3365cd4562f5a91d5cf0f3ca4dae6130f6675c0
JulienBreton <julienbreton@hotmail.fr>
2020-01-26 21:24:44 +0100

    dev(Qt): gére menu contextuel en fr

--- Qt/ZeroClassGenerator/main.cpp
+++ Qt/ZeroClassGenerator/main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QString locale = QLocale::system().name().section('_', 0, 0);
+    QTranslator translator;
+    translator.load(QString("qt_") + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&translator);
+
     FenPrincipale * FenetrePrincipale = new FenPrincipale();
     FenetrePrincipale->show();
 
