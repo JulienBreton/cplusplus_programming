125e1a943ad809339196d2440cb1ebafc1378b0f
JulienBreton <julienbreton@hotmail.fr>
2020-01-28 21:34:57 +0100

    dev(Qt): finalisation TP

--- Qt/ZeroClassGenerator/fenprincipale.cpp
+++ Qt/ZeroClassGenerator/fenprincipale.cpp
@@ -160,15 +160,15 @@ void FenPrincipale::genererCode()
             if(m_ajouterLicenceGPL->isChecked())
             {
                 codeGenere += "\nLicence GPL : \n\n";
-                codeGenere += "This program is free software: you can redistribute it and/or modify"
-                              "it under the terms of the GNU General Public License as published by"
-                              "the Free Software Foundation, either version 3 of the License, or"
-                              "(at your option) any later version.\n\n"
-                              "This program is distributed in the hope that it will be useful,"
-                              "but WITHOUT ANY WARRANTY; without even the implied warranty of"
-                              "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-                              "GNU General Public License for more details.\n\n"
-                              "You should have received a copy of the GNU General Public License"
+                codeGenere += "This program is free software: you can redistribute it and/or modify "
+                              "it under the terms of the GNU General Public License as published by "
+                              "the Free Software Foundation, either version 3 of the License, or "
+                              "(at your option) any later version.\n"
+                              "This program is distributed in the hope that it will be useful, "
+                              "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+                              "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See th e"
+                              "GNU General Public License for more details.\n"
+                              "You should have received a copy of the GNU General Public License "
                               "along with this program.  If not, see <https://www.gnu.org/licenses/>";
                 codeGenere += "\n";
             }
@@ -224,7 +224,7 @@ void FenPrincipale::genererCode()
 
             if(!m_classeMere->text().isEmpty())
             {
-                codeGenere += " : ";
+                codeGenere += " : public ";
                 codeGenere += m_classeMere->text();
             }
 
